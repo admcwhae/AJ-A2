eb9dfd29951da62bfc54c7bdc4471692157736a6
Moiz <Moiz@10.1.59.240>
2018-10-18 15:59:48 +1100

    Database/Tables Creation and Menu Table Loaded

--- AdvancedJava_A2_ClientServer/src/advancedjava_a2/CustomerFXMLController.java
+++ AdvancedJava_A2_ClientServer/src/advancedjava_a2/CustomerFXMLController.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,7 +37,7 @@ import javafx.scene.input.MouseEvent;
  * @author akbar
  */
 public class CustomerFXMLController implements Initializable {
-
+        
     OrderSystem orderSystem;
     
     //first partition - "customer details"
@@ -91,8 +92,44 @@ public class CustomerFXMLController implements Initializable {
      * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) {       
+        
+        //Create the database
+        final String DATABASE_ORDER_SYSTEM_DB_QRY = "CREATE DATABASE IF NOT EXISTS orderSystemDb";
+        DatabaseUtility.performStatement(DATABASE_ORDER_SYSTEM_DB_QRY);
+        
+        //Drop the old menu table
+        final String DROP_TABLE_MENU_QRY = "DROP TABLE IF EXISTS `orderSystemDb`.`menu`";     
+        DatabaseUtility.performStatement(DROP_TABLE_MENU_QRY);
+        
+        //Create the menu table
+        final String TABLE_MENU_QRY = "CREATE TABLE IF NOT EXISTS `orderSystemDb`.`menu` ( `menuId` INT NOT NULL , `type` VARCHAR(10) NOT NULL , `mealType` VARCHAR(10) NOT NULL , `name` VARCHAR(100) NOT NULL , `price` DOUBLE NOT NULL , `energy` DOUBLE NOT NULL , `protein` DOUBLE NOT NULL , `carbohydrates` DOUBLE NOT NULL , `fat` DOUBLE NOT NULL , `fibre` DOUBLE NOT NULL , PRIMARY KEY (`menuId`))";
+        DatabaseUtility.performStatement(TABLE_MENU_QRY);
+        
+        //Create the orders table
+        final String TABLE_ORDERS_QRY = "CREATE TABLE IF NOT EXISTS `orderSystemDb`.`orders` ( `orderId` INT NOT NULL AUTO_INCREMENT, `customerName` VARCHAR(30) NOT NULL , `tableNumber` INT(10) NOT NULL , `foodItem` VARCHAR(100) NOT NULL REFERENCES menu(name) , `beverageItem` VARCHAR(100) NOT NULL REFERENCES menu(name), `status` VARCHAR(100) NOT NULL, PRIMARY KEY (orderId));";
+        DatabaseUtility.performStatement(TABLE_ORDERS_QRY);
+
+        //Getting all Menu Items
+        ArrayList<MenuItem> menuItemsList = menu.getMenuItemsList();
+        
+        //Inserting all Menu Items in database
+        for(MenuItem item : menuItemsList)
+        {
+            DatabaseUtility.performStatement("INSERT INTO menu (`menuId`, `type`, `mealType`, `name`, `price`, `energy`, `protein`, `carbohydrates`, `fat`, `fibre`) VALUES ('" + 
+                    item.getMenuItemId() + "', '" +                    
+                    item.getMenuDescription() + "', '" +
+                    item.getMealType() + "', '" +
+                    item.getItemName() + "', '" +
+                    item.getPrice() + "', '" +
+                    item.getEnergy() + "', '" +
+                    item.getProtein() + "', '" +
+                    item.getCarbohydrates() + "', '" +
+                    item.getFat() + "', '" +                    
+                    item.getDietaryFibre() + "');" 
+            );
+        }
+                        
         
         //Assign toggleGroup to radio buttons
         setRadioButtonToggleGroup();
